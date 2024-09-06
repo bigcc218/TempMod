4bc1410b7c638d4fd4099fedea58e13e7514e1af
bigcc218 <bigcc218@hotmail.com>
2024-09-06 20:35:20 +0800

    TempMod

--- src/main/java/name/temp_mod/registry/ModItems.java
+++ src/main/java/name/temp_mod/registry/ModItems.java
@@ -1,5 +1,29 @@
 package name.temp_mod.registry;
 
+import jdk.jfr.Registered;
+import name.temp_mod.TempMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
 public class ModItems {
 
+    public static final Item SITRINE = registerItem("sitrine", new Item(new FabricItemSettings()));
+    public static final Item RAW_SITRINE = registerItem("raw_sitrine", new Item(new FabricItemSettings()));
+
+    public static Item registerItem(String name, Item item, ItemGroup... itemGroups) {
+        Item registeredItem = Registry.register(Registries.ITEM, new Identifier(TempMod.MOD_ID, name), item);
+        for (ItemGroup itemGroup : itemGroups) {
+            ItemGroupEvents.modifyEntriesEvent(itemGroup).register(entries -> entries.add(registeredItem));
+        }
+        return registeredItem;
+    }
+
+    public static void registerModItems() {
+        TempMod.LOGGER.debug("Registering mod item for" + TempMod.MOD_ID);
+    }
 }
