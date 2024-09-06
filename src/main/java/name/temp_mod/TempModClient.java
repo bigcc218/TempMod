3bf64d8cd80260a34c9db408269b883058ced277
bigcc218 <bigcc218@hotmail.com>
2024-09-06 20:06:57 +0800

    TempMod

---
+++ src/main/java/name/temp_mod/TempModClient.java
@@ -0,0 +1,10 @@
+package name.temp_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TempModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
